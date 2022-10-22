2b3ad4f950a146e0004dedba6b404a038dd205ac
AFynjy <avenchakov@yandex.ru>
2022-10-22 15:28:32 +0300

    firsxt commit

---
+++ src/components/FormCards.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import MySelect from "./MySelect";
+
+const FormCards = () => {
+  return (
+    <>
+      <MySelect />
+      <div className="container">
+        <div className="container-1">
+          <input />
+        </div>
+        <div className="container-2">
+          <input />
+        </div>
+        <div className="container-3">
+          <input />
+        </div>
+        <div className="container-4">
+          <input />
+        </div>
+        <div className="container-5">
+          <input />
+        </div>
+        <div className="container-6">
+          <input />
+        </div>
+        <div className="container-7">
+          <input />
+        </div>
+        <div className="container-8">
+          <input />
+        </div>
+        <div className="container-9">
+          <input />
+        </div>
+        <div className="container-10">
+          <input />
+        </div>
+        <div className="container-11">
+          <input />
+        </div>
+        <div className="container-12">
+          <input />
+        </div>
+        <div className="container-13">
+          <input />
+        </div>
+        <div className="container-14">
+          <input />
+        </div>
+        <div className="container-15">
+          <input />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FormCards;
---
+++ src/components/Table.jsx
@@ -0,0 +1,115 @@
+import React, {useState} from "react";
+import Modal from "./Modal";
+import FormCard from "./FormCard";
+import FormCards from "./FormCards";
+
+const Table = () => {
+  const [modal, setModal] = useState(false)
+  const data = [
+    {
+      regNomer: "111",
+      dataPostupleniyaVMKS: "11/2/22",
+      nameDoc: "ТЗ в рамках согласования распоряжения",
+      addInf: "",
+      FOIV: "Федеральный фонд обязательного медицинского страхования",
+      curatorAPRF: "Иванов",
+      direction: "Здравоохранение",
+      fedProj: "ГИС ГМП",
+      typeDoc: "ТЗ",
+      solutionDoc: "Согласовано с учётом замечаний",
+      currentStatus: "Исполнено",
+      finGrade: 50,
+      expertGrade: 100,
+      requisitesAnsw: 333,
+      dateAnsw: "7/4/22",
+    },
+    {
+      regNomer: "222",
+      dataPostupleniyaVMKS: "11/2/22",
+      nameDoc: "ТЗ в рамках согласования распоряжения",
+      addInf: "",
+      FOIV: "Федеральный фонд обязательного медицинского страхования",
+      curatorAPRF: "Иванов",
+      direction: "Здравоохранение",
+      fedProj: "ГИС ГМП",
+      typeDoc: "ТЗ",
+      solutionDoc: "Согласовано с учётом замечаний",
+      currentStatus: "Исполнено",
+      finGrade: 50,
+      expertGrade: 100,
+      requisitesAnsw: 333,
+      dateAnsw: "7/4/22",
+    },
+    {
+      regNomer: 33,
+      dataPostupleniyaVMKS: "11/2/22",
+      nameDoc: "ТЗ в рамках согласования распоряжения",
+      addInf: "",
+      FOIV: "Федеральный фонд обязательного медицинского страхования",
+      curatorAPRF: "Иванов",
+      direction: "Здравоохранение",
+      fedProj: "ГИС ГМП",
+      typeDoc: "ТЗ",
+      solutionDoc: "Согласовано с учётом замечаний",
+      currentStatus: "Исполнено",
+      finGrade: 50,
+      expertGrade: 100,
+      requisitesAnsw: 333,
+      dateAnsw: "7/4/22",
+    },
+  ];
+
+  const namesColumn = [
+    "Рег. номер МКС в СЭД",
+    "Дата поступления в МКС",
+    "Название документа",
+    "В дополнение к или взамен (предыстория документа)",
+    "ФОИВ",
+    "Куратор АПРФ",
+    "Направление",
+    "Фед. Проект / ГИС",
+    "Тип документа",
+    "Решение по документу",
+    "Текущий статус",
+    "Финансовая оценка, тыс. руб.",
+    "Экспертная оценка, тыс. руб.",
+    "Реквизиты ответа в ведомство",
+    "Дата ответа в ведомство",
+  ];
+
+  return (
+    <>
+      <div> {modal && <Modal onClose={() => setModal(false)} title="Форма карточки" ><FormCards /></Modal>}</div>
+      <table>
+        <tr>
+          {namesColumn.map((nameColumn, key) => {
+            return <th key={key}>{nameColumn}</th>;
+          })}
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr onClick={() => setModal(true)} key={key}>
+              <td>{val.regNomer}</td>
+              <td>{val.dataPostupleniyaVMKS}</td>
+              <td>{val.nameDoc}</td>
+              <td>{val.addInf}</td>
+              <td>{val.FOIV}</td>
+              <td>{val.curatorAPRF}</td>
+              <td>{val.direction}</td>
+              <td>{val.fedProj}</td>
+              <td>{val.typeDoc}</td>
+              <td>{val.solutionDoc}</td>
+              <td>{val.currentStatus}</td>
+              <td>{val.finGrade}</td>
+              <td>{val.expertGrade}</td>
+              <td>{val.requisitesAnsw}</td>
+              <td>{val.dateAnsw}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </>
+  );
+};
+
+export default Table;
